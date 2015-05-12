1412de9bf73354101a3826317d1b23dfa7177ebd
Iain Kruger <iain.kruger@gmail.com>
2015-05-12 22:15:17 +0200

    model calculations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ActiveRecord::Base
+
+def weeks_to_event
+    self.date.strftime("%U").to_i - Date.today.strftime("%U").to_i
+end
+
+def week_number
+	(self.date -  Date.today).to_i
+end
+
 end
