b5e014e0366b695e2627fbefae60444253a56e70
Iain Kruger <iain.kruger@gmail.com>
2015-05-30 16:48:37 +0200

    weeks and days

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,7 +23,34 @@ def odd_even
 		"even"
 	end
 	
-	
+def weeks_left
+	if days_to_event.to_i <=7 then
+		0 
+		
+	elsif days_to_event
+		weeks_to_event - 1
+		
+	end
+end
+
+def days_left
+	daysLeft = days_to_event - (weeks_left * 7)
+	days = 0
+	if daysLeft == 7 then 
+		days.to_i	
+	else
+		daysLeft.to_i
+	end
+end
+
+def weeks_left_final
+	if days_left == 0 then
+		weeks_to_event
+	else
+		weeks_to_event - 1
+	end
+end
+
 end
 
 end
