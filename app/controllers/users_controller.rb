b5c25deb369e11b653993cc68d4c8df26004def4
Iain Kruger <iain.kruger@gmail.com>
2015-06-23 16:17:12 +0200

    new rout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   
   
   def new
+    if logged_in?
+      user = current_user
+      redirect_to user
+    else
   	@user= User.new
+  end
+
   end
 
   def index
